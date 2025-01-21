59d4d6b9addb52039ecfbf7b6597cb05343bafd0
Joan M. Flores Lopez <joamanuel@gmail.com>
2025-01-21 10:10:00 -0700

    Added subtract operation

--- src/goph420_lab00/operators.py
+++ src/goph420_lab00/operators.py
@@ -50,3 +50,20 @@ def divide(x, y):
     The (element-wise) quotient of x and y.
     """
     return x/y
+
+def subtract(x, y):
+    """Suctract two numbers or arrays.
+    
+    Parameters
+    ----------
+    x: int or float or array_like
+    The first number to subtract.
+    y: int or float or array_like
+    The second number to subtract.
+    
+    Returns
+    -------
+    int or float or array_like
+    The (element-wise) subtraction of x and y.
+    """
+    return x - y
